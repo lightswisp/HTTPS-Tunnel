6987dc88a5d575f9113061d891ad24fdb2bea3d6
lightswisp <lightswisp@yandex.ru>
2023-05-12 05:30:35 -0400

    more fixes..

--- server.rb
+++ server.rb
@@ -12,6 +12,7 @@ CONN_OK = "HTTP/1.1 200 Established\r\nDate: #{Time.now}\r\nServer: #{SERVER_NAM
 CONN_FAIL = "HTTP/1.1 502 Bad Gateway\r\nDate: #{Time.now}\r\nServer: #{SERVER_NAME}\r\n\r\n"
 TTL = 10 # 10 seconds io select timeout
 TCP_CONN_TIME_ABORT = 10 # 10 seconds timeout to abort the connection if tls connection is still in the PINIT state. This feature was made to drop all tcp connections that didn't start the tls negotiation process 
+OPTIONS = {}
 
 # SSL Configuration
 SSL = {
@@ -25,19 +26,17 @@ SSL = {
          :SSLVerifyDepth=>5,                            
          :SSLVerifyCallback=>nil,
          :SSLTimeout=>2,                                
-         :SSLOptions=>nil,                                
+         :SSLOPTIONS=>nil,                                
          :SSLCiphers=>nil,                                
          :SSLStartImmediately=>true,                      
          :SSLCertName=>nil,   
          :SSLVer=>OpenSSL::SSL::TLS1_3_VERSION 
 }
 
-
-options = {}
 OptionParser.new do |opts|
-	opts.banner = "TLS Tunnel Server\n\n".bold + "Usage: ./server.rb [options]"
+	opts.banner = "TLS Tunnel Server\n\n".bold + "Usage: ./server.rb [OPTIONS]"
 		opts.on("-v", "--verbose", "Run verbosely") do |v|
-		options[:verbose] = v
+		OPTIONS[:verbose] = v
 	end
 
 	opts.on("-h", "--help", "Prints help") do
@@ -46,10 +45,10 @@ OptionParser.new do |opts|
 	end
 
 	opts.on("-cCERT", "--certificate=CERT", "SSL Certificate, example: ./server.rb --certificate certificate.crt --key private.key") do |cert|
-		options[:cert] = cert
+		OPTIONS[:cert] = cert
 	end
 	opts.on("-kKEY", "--key=KEY", "Private key, example: ./server.rb --certificate certificate.crt --key private.key") do |key|
-		options[:key] = key
+		OPTIONS[:key] = key
 	end
 
 end.parse!
@@ -59,12 +58,12 @@ if Process.uid != 0
 	exit
 end
 
-if !options[:cert] || !options[:key]
+if !OPTIONS[:cert] || !OPTIONS[:key]
 	puts "Please provide your ssl certificate and private key, example: ./server.rb --certificate certificate.crt --key private.key".red
 	exit
 end
 
-if !File.exist?(options[:cert]) || !File.exist?(options[:key])
+if !File.exist?(OPTIONS[:cert]) || !File.exist?(OPTIONS[:key])
 	puts "SSL Certificate or private key not found! Please double check your local directory".red
 	exit
 end
@@ -74,8 +73,8 @@ socket = TCPServer.new(PORT)
 puts "Listening on #{PORT}".bold
 
 sslContext 					= OpenSSL::SSL::SSLContext.new()
-sslContext.cert             = OpenSSL::X509::Certificate.new(File.open(options[:cert]))
-sslContext.key              = OpenSSL::PKey::RSA.new(File.open(options[:key]))
+sslContext.cert             = OpenSSL::X509::Certificate.new(File.open(OPTIONS[:cert]))
+sslContext.key              = OpenSSL::PKey::RSA.new(File.open(OPTIONS[:key]))
 sslContext.verify_mode      = SSL[:SSLVerifyClient]
 sslContext.verify_depth     = SSL[:SSLVerifyDepth]
 sslContext.timeout          = SSL[:SSLTimeout]
@@ -83,25 +82,25 @@ sslContext.min_version      = SSL[:SSLVer] # *IMPORTANT* TLS_1.3
 
 
 def handle_client(connection)
-        puts "[*] New connection #{connection.peeraddr[-1]}:#{connection.peeraddr[1]}"
+        puts "[*] New connection #{connection.peeraddr[-1]}:#{connection.peeraddr[1]}" if OPTIONS[:verbose]
         address = connection.gets
         if address.match?(/GET/) # if it's not the address but the actual request from the client's browser
 	        response = "HTTP/1.1 200 OK\r\nServer: #{SERVER_NAME}\r\nContent-Type: text/html\r\n\r\n#{File.read('index.html')}"
 	        connection.puts(response)
 	        connection.close
-	        puts "[LOGS] Webpage is shown, closing the connection...".green if options[:verbose]
+	        puts "[LOGS] Webpage is shown, closing the connection...".green if OPTIONS[:verbose]
 	        Thread.exit
         end
         begin
 	        endpoint_host = address.split(":")[0]
 	        endpoint_port = address.split(":")[1].to_i
         rescue
-	        puts "[WARNING] Client doesn't know how to communicate with us!" if options[:verbose]
+	        puts "[WARNING] Client doesn't know how to communicate with us!" if OPTIONS[:verbose]
 	        connection.close if connection
 	        Thread.exit
         end
 
-        puts "#{endpoint_host}:#{endpoint_port}".green
+        puts "#{endpoint_host}:#{endpoint_port}".green if OPTIONS[:verbose]
         endpoint_connection = TCPSocket.new(endpoint_host, endpoint_port)
         endpoint_connection.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true) 
 
@@ -125,7 +124,7 @@ def handle_client(connection)
 				end
 			end
 		rescue
-			puts "[*] Closing connection with #{endpoint_host}:#{endpoint_port}".red if options[:verbose]
+			puts "[*] Closing connection with #{endpoint_host}:#{endpoint_port}".red if OPTIONS[:verbose]
 			endpoint_connection.close() if endpoint_connection
 			connection.close() if connection
 			Thread.exit
@@ -150,7 +149,7 @@ loop do
 			  connection.close                
 			end
 		rescue => e
-			puts "[ERROR] #{e}".red if options[:verbose]
+			puts "[ERROR] #{e}".red if OPTIONS[:verbose]
 			connection.close if connection
 		end
 
