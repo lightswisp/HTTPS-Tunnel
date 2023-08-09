1048c250ca34c3d49bc2513a03b9b2b04a334560
Your Name <you@example.com>
2023-08-08 22:31:21 -0400

    fix server

--- client.rb
+++ client.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
 require 'socket'
-require 'thread'
 require 'openssl'
 require 'colorize'
 require 'optparse'
@@ -10,152 +9,148 @@ ARGV << '-h' if ARGV.empty?
 
 options = {}
 OptionParser.new do |opts|
-	opts.banner = "TLS Tunnel Client\n\n".bold + "Usage: ./client.rb [options]"
-		opts.on("-v", "--verbose", "Run verbosely") do |v|
-		options[:verbose] = v
-	end
+  opts.banner = "TLS Tunnel Client\n\n".bold + 'Usage: ./client.rb [options]'
+  opts.on('-v', '--verbose', 'Run verbosely') do |v|
+    options[:verbose] = v
+  end
 
-	opts.on("-h", "--help", "Prints help") do
-		puts opts
-		exit
-	end
+  opts.on('-h', '--help', 'Prints help') do
+    puts opts
+    exit
+  end
 
-	opts.on("-aADDR", "--address=ADDR", "TLS Tunnel Server address, example: ./client.rb --address example.com") do |addr|
-		options[:addr] = addr
-	end
+  opts.on('-aADDR', '--address=ADDR',
+          'TLS Tunnel Server address, example: ./client.rb --address example.com') do |addr|
+    options[:addr] = addr
+  end
 
-	opts.on("-fKEY", "--auth-keyfile=KEY", "TLS Tunnel Server authorization key file, example: ./client.rb --auth-keyfile auth_key.txt") do |auth_key|
-		options[:auth_key] = auth_key
-	end
+  opts.on('-fKEY', '--auth-keyfile=KEY',
+          'TLS Tunnel Server authorization key file, example: ./client.rb --auth-keyfile auth_key.txt') do |auth_key|
+    options[:auth_key] = auth_key
+  end
 
-	opts.on("-pPORT", "--port=PORT", "Local port for listening, example: ./client.rb --address example.com --port 8080") do |port|
-		options[:port] = port
-	end
+  opts.on('-pPORT', '--port=PORT',
+          'Local port for listening, example: ./client.rb --address example.com --port 8080') do |port|
+    options[:port] = port
+  end
 
-	opts.on("-sSNI", "--sni=SNI", "TLS SNI extension spoof, default is [ example.com ] (optional)") do |sni|
-		options[:sni] = sni
-	end
-
-
+  opts.on('-sSNI', '--sni=SNI', 'TLS SNI extension spoof, default is [ example.com ] (optional)') do |sni|
+    options[:sni] = sni
+  end
 end.parse!
 
 if !options[:addr] || !options[:port] || !options[:auth_key]
-	puts "Please include the server address, authorization key and local port for listening!\nExample: ./client.rb --address/-a example.com --port/-p 8080 --auth-keyfile/-f auth_key.txt".red
-	exit
+  puts "Please include the server address, authorization key and local port for listening!\nExample: ./client.rb --address/-a example.com --port/-p 8080 --auth-keyfile/-f auth_key.txt".red
+  exit
 end
 
-if !File.exist?(options[:auth_key])
-	puts "Authentication key file not found!".red
-	exit
+unless File.exist?(options[:auth_key])
+  puts 'Authentication key file not found!'.red
+  exit
 end
 
 PROXY_PORT  = options[:port]
 SERVER_HOST = options[:addr]
-SERVER_PORT = 443 							 # Don't change this, because the server we are connecting to is supposed to immitate the real https connection
-SNI_HOST 	= options[:sni] || "example.com" # SNI SPOOFING
-TTL		 	= 10 							 # 10 SEC
-MAX_BUFFER  = 1024 * 640 					 # 640KB
+SERVER_PORT = 443 # Don't change this, because the server we are connecting to is supposed to immitate the real https connection
+SNI_HOST	= options[:sni] || 'example.com' # SNI SPOOFING
+TTL	= 10 # 10 SEC
+MAX_BUFFER = 1024 * 640 # 640KB
 AUTH_KEY	= File.read(options[:auth_key]).chomp
 
 def connect(host, port)
-	begin
-		socket = TCPSocket.new(host, port)   
-		return nil if !socket
-		sslContext = OpenSSL::SSL::SSLContext.new
-		sslContext.min_version = OpenSSL::SSL::TLS1_3_VERSION
-		ssl = OpenSSL::SSL::SSLSocket.new(socket, sslContext)
-		ssl.hostname = SNI_HOST
-		ssl.sync_close = true
-		ssl.connect 
-	rescue
-		puts "[WARNING] TLS Tunnel Server seems to be down".red
-		return nil
-	end
-	return ssl
+  begin
+    socket = TCPSocket.new(host, port)
+    return nil unless socket
+
+    sslContext = OpenSSL::SSL::SSLContext.new
+    sslContext.min_version = OpenSSL::SSL::TLS1_3_VERSION
+    ssl = OpenSSL::SSL::SSLSocket.new(socket, sslContext)
+    ssl.hostname = SNI_HOST
+    ssl.sync_close = true
+    ssl.connect
+  rescue StandardError
+    puts '[WARNING] TLS Tunnel Server seems to be down'.red
+    return nil
+  end
+  ssl
 end
 
 def is_alive?(ssl, payload)
-	payload = payload.gsub("\r\n\r\n", "\r\nAuthorization: #{AUTH_KEY}\r\n\r\n")
-	ssl.puts(payload)
-	begin
-	  response = ssl.readpartial(MAX_BUFFER) 
-	rescue
-		return nil
-	end
-	return response
+  payload = payload.gsub("\r\n\r\n", "\r\nAuthorization: #{AUTH_KEY}\r\n\r\n")
+  ssl.puts(payload)
+  begin
+    response = ssl.readpartial(MAX_BUFFER)
+  rescue StandardError
+    return nil
+  end
+  response
 end
-
 
 proxy = TCPServer.new(PROXY_PORT)
 puts "[#{Time.now}] Listening on #{PROXY_PORT}".bold
 
 loop do
-	connection = proxy.accept
-	Thread.new {
-		request = connection.recv(MAX_BUFFER)
-		Thread.exit if request.size < 1 || request.empty? || request.nil?
-		request_head = request.split("\r\n")
-		request_method = request_head.first.split(" ")[0] # CONNECT, GET, POST
+  connection = proxy.accept
+  Thread.new do
+    request = connection.recv(MAX_BUFFER)
+    Thread.exit if request.size < 1 || request.empty? || request.nil?
+    request_head = request.split("\r\n")
+    request_method = request_head.first.split(' ')[0] # CONNECT, GET, POST
 
-		if request_method =~ /CONNECT/
-			ssl = connect(SERVER_HOST, SERVER_PORT)
-			Thread.exit if !ssl
-			request_host, request_port = request_head.first.split(" ")[1].split(":")
-			if header = is_alive?(ssl, request)
-				puts "[CONNECT] #{request_host}:#{request_port}".green if options[:verbose]
-				connection.puts(header)
-			else
-				puts "[CONNECT] #{request_host}:#{request_port} is unavailable!".red if options[:verbose]
-				ssl.close
-				connection.close
-				Thread.exit
-			end
-			
+    if request_method =~ /CONNECT/
+      ssl = connect(SERVER_HOST, SERVER_PORT)
+      Thread.exit unless ssl
+      request_host, request_port = request_head.first.split(' ')[1].split(':')
+      if header = is_alive?(ssl, request)
+        puts "[CONNECT] #{request_host}:#{request_port}".green if options[:verbose]
+        connection.puts(header)
+      else
+        puts "[CONNECT] #{request_host}:#{request_port} is unavailable!".red if options[:verbose]
+        ssl.close
+        connection.close
+        Thread.exit
+      end
 
-			begin
+      begin
+        loop do
+          fds = IO.select([connection, ssl], nil, nil, TTL)
+          if fds[0].member?(connection)
+            buf = connection.readpartial(MAX_BUFFER)
+            ssl.print(buf)
+          elsif fds[0].member?(ssl)
+            buf = ssl.readpartial(MAX_BUFFER)
+            connection.print(buf)
+          end
+        end
+      rescue StandardError
+        puts "[INFO] Closing connection with #{request_host}:#{request_port}".red if options[:verbose]
+        ssl.close if ssl
+        connection.close if connection
+        Thread.exit
+      end
 
-			     loop do
-                        fds = IO.select([connection, ssl], nil, nil, TTL)
-                        if fds[0].member?(connection)
-                                buf = connection.readpartial(MAX_BUFFER)
-                                ssl.print(buf)
-                        elsif fds[0].member?(ssl)
-                                buf = ssl.readpartial(MAX_BUFFER)
-                                connection.print(buf)
-                        end
+    else
+      # GET POST PUT POST PATCH DELETE ETC#
+      ssl = connect(SERVER_HOST, SERVER_PORT)
+      Thread.exit unless ssl
 
-                end
+      method = request.split("\n")[0]
+      host = request.split("\n")[1].downcase.gsub('host:', '').strip
+      request_host, request_port = host.split(':')
+      request_port = 80 if request_port.nil?
+      request_port = request_port.to_i
 
-			rescue
-				puts "[INFO] Closing connection with #{request_host}:#{request_port}".red if options[:verbose]
-				ssl.close if ssl
-				connection.close if connection
-				Thread.exit
-			end
-
-		else
-			# GET POST PUT POST PATCH DELETE ETC#
-			ssl = connect(SERVER_HOST, SERVER_PORT)
-			Thread.exit if !ssl
-
-			method = request.split("\n")[0]
-			host = request.split("\n")[1].downcase.gsub("host:", "").strip
-            request_host, request_port = host.split(":")
-            request_port = 80 if request_port.nil?
-            request_port = request_port.to_i
-
-			request = request.gsub("\r\n\r\n", "\r\nAuthorization: #{AUTH_KEY}\r\n\r\n")
-			ssl.puts(request)
-			begin
-				response = ssl.readpartial(MAX_BUFFER)
-				connection.puts(response)
-				puts "[NON-CONNECT] #{request_host}:#{request_port}".green if options[:verbose]
-			rescue
-				puts "[NON-CONNECT] #{request_host}:#{request_port}".red if options[:verbose]
-			ensure
-				connection.close
-			end
-		end
-			
-	}
+      request = request.gsub("\r\n\r\n", "\r\nAuthorization: #{AUTH_KEY}\r\n\r\n")
+      ssl.puts(request)
+      begin
+        response = ssl.readpartial(MAX_BUFFER)
+        connection.puts(response)
+        puts "[NON-CONNECT] #{request_host}:#{request_port}".green if options[:verbose]
+      rescue StandardError
+        puts "[NON-CONNECT] #{request_host}:#{request_port}".red if options[:verbose]
+      ensure
+        connection.close
+      end
+    end
+  end
 end
--- server.rb
+++ server.rb
@@ -1,221 +1,219 @@
 #!/usr/bin/ruby
 require 'socket'
 require 'openssl'
-require 'thread'
 require 'colorize'
 require 'timeout'
 require 'optparse'
 
 PUBLIC_IP = `curl -s ifconfig.me`
-MAX_BUFFER  = 1024 * 640 # 640KB
+MAX_BUFFER = 1024 * 640 # 640KB
 PORT = 443 # Don't change this, because this server imitates the real https server
 SERVER_NAME = "Rubinius_1.0.16/(#{RUBY_VERSION})/(#{OpenSSL::OPENSSL_VERSION})"
 CONN_OK = "HTTP/1.1 200 Established\r\nDate: #{Time.now}\r\nServer: #{SERVER_NAME}\r\n\r\n"
 CONN_FAIL = "HTTP/1.1 502 Bad Gateway\r\nDate: #{Time.now}\r\nServer: #{SERVER_NAME}\r\n\r\n<h1>502 Bad Gateway</h1>"
 TTL = 10 # 10 seconds io select timeout
-TCP_CONN_TIME_ABORT = 10 # 10 seconds timeout to abort the connection if tls connection is still in the PINIT state. This feature was made to drop all tcp connections that didn't start the tls negotiation process 
+TCP_CONN_TIME_ABORT = 10 # 10 seconds timeout to abort the connection if tls connection is still in the PINIT state. This feature was made to drop all tcp connections that didn't start the tls negotiation process
 OPTIONS = {}
 
 # SSL Configuration
 SSL = {
-         :SSLClientCA=>nil,
-         :SSLExtraChainCert=>nil,                         
-         :SSLCACertificateFile=>nil,                      
-         :SSLCACertificatePath=>nil,                      
-         :SSLCertificateStore=>nil,                       
-         :SSLTmpDhCallback=>nil,                          
-         :SSLVerifyClient=>OpenSSL::SSL::VERIFY_PEER,                             
-         :SSLVerifyDepth=>5,                            
-         :SSLVerifyCallback=>nil,
-         :SSLTimeout=>2,                                
-         :SSLOPTIONS=>nil,                                
-         :SSLCiphers=>nil,                                
-         :SSLStartImmediately=>true,                      
-         :SSLCertName=>nil,   
-         :SSLVer=>OpenSSL::SSL::TLS1_3_VERSION 
+  SSLClientCA: nil,
+  SSLExtraChainCert: nil,
+  SSLCACertificateFile: nil,
+  SSLCACertificatePath: nil,
+  SSLCertificateStore: nil,
+  SSLTmpDhCallback: nil,
+  SSLVerifyClient: OpenSSL::SSL::VERIFY_PEER,
+  SSLVerifyDepth: 5,
+  SSLVerifyCallback: nil,
+  SSLTimeout: 2,
+  SSLOPTIONS: nil,
+  SSLCiphers: nil,
+  SSLStartImmediately: true,
+  SSLCertName: nil,
+  SSLVer: OpenSSL::SSL::TLS1_3_VERSION
 }
 
 OptionParser.new do |opts|
-        opts.banner = "TLS Tunnel Server\n\n".bold + "Usage: ./server.rb [OPTIONS]"
-                opts.on("-v", "--verbose", "Run verbosely") do |v|
-                OPTIONS[:verbose] = v
-        end
+  opts.banner = "TLS Tunnel Server\n\n".bold + 'Usage: ./server.rb [OPTIONS]'
+  opts.on('-v', '--verbose', 'Run verbosely') do |v|
+    OPTIONS[:verbose] = v
+  end
 
-        opts.on("-h", "--help", "Prints help") do
-                puts opts
-                exit
-        end
+  opts.on('-h', '--help', 'Prints help') do
+    puts opts
+    exit
+  end
 
-        opts.on("-fKEY", "--auth-keyfile=KEY", "TLS Tunnel Server authorization key file, example: ./server.rb --auth-keyfile auth_key.txt") do |auth_key|
-		OPTIONS[:auth_key] = auth_key
-		end
+  opts.on('-fKEY', '--auth-keyfile=KEY',
+          'TLS Tunnel Server authorization key file, example: ./server.rb --auth-keyfile auth_key.txt') do |auth_key|
+    OPTIONS[:auth_key] = auth_key
+  end
 
-        opts.on("-cCERT", "--certificate=CERT", "SSL Certificate, example: ./server.rb --certificate certificate.crt --key private.key") do |cert|
-                OPTIONS[:cert] = cert
-        end
-        opts.on("-kKEY", "--key=KEY", "Private key, example: ./server.rb --certificate certificate.crt --key private.key") do |key|
-                OPTIONS[:key] = key
-        end
-
+  opts.on('-cCERT', '--certificate=CERT',
+          'SSL Certificate, example: ./server.rb --certificate certificate.crt --key private.key') do |cert|
+    OPTIONS[:cert] = cert
+  end
+  opts.on('-kKEY', '--key=KEY',
+          'Private key, example: ./server.rb --certificate certificate.crt --key private.key') do |key|
+    OPTIONS[:key] = key
+  end
 end.parse!
 
 if Process.uid != 0
-        puts "You must run it as root!".red
-        exit
+  puts 'You must run it as root!'.red
+  exit
 end
 
 if !OPTIONS[:cert] || !OPTIONS[:key] || !OPTIONS[:auth_key]
-        puts "Please provide your ssl certificate, authentication key file and private key!\nExample: ./server.rb --certificate/-c certificate.crt --key/-k private.key --auth-keyfile/-f auth_key.txt".red
-        exit
+  puts "Please provide your ssl certificate, authentication key file and private key!\nExample: ./server.rb --certificate/-c certificate.crt --key/-k private.key --auth-keyfile/-f auth_key.txt".red
+  exit
 end
 
 if !File.exist?(OPTIONS[:cert]) || !File.exist?(OPTIONS[:key]) || !File.exist?(OPTIONS[:auth_key])
-        puts "SSL Certificate or private key or authentication key not found! Please double check your local directory".red
-        exit
+  puts 'SSL Certificate or private key or authentication key not found! Please double check your local directory'.red
+  exit
 end
 
 AUTH_KEY = File.read(OPTIONS[:auth_key]).chomp
 socket = TCPServer.new(PORT)
 puts "[#{Time.now}] Listening on #{PORT}".bold
 
-sslContext                                      = OpenSSL::SSL::SSLContext.new()
+sslContext = OpenSSL::SSL::SSLContext.new
 sslContext.cert             = OpenSSL::X509::Certificate.new(File.open(OPTIONS[:cert]))
 sslContext.key              = OpenSSL::PKey::RSA.new(File.open(OPTIONS[:key]))
 sslContext.verify_mode      = SSL[:SSLVerifyClient]
 sslContext.verify_depth     = SSL[:SSLVerifyDepth]
 sslContext.timeout          = SSL[:SSLTimeout]
-sslContext.min_version      = SSL[:SSLVer] # *IMPORTANT* TLS_1.3 
-
+sslContext.min_version      = SSL[:SSLVer] # *IMPORTANT* TLS_1.3
 
 def handle_client(connection)
-        puts "[*] New connection #{connection.peeraddr[-1]}:#{connection.peeraddr[1]}" if OPTIONS[:verbose]
-        request = connection.readpartial(MAX_BUFFER)
+  puts "[*] New connection #{connection.peeraddr[-1]}:#{connection.peeraddr[1]}" if OPTIONS[:verbose]
+  request = connection.readpartial(MAX_BUFFER)
 
-		if(request.nil? || request.empty?)
-			puts "[WARNING] Empty request!" if OPTIONS[:verbose]
-			connection.close if connection
-			Thread.exit
-		end
-		
-		request_split = request.split("\r\n")
+  if request.nil? || request.empty?
+    puts '[WARNING] Empty request!' if OPTIONS[:verbose]
+    connection.close if connection
+    Thread.exit
+  end
 
-                if(request.match?(/CONNECT/))
+  request_split = request.split("\r\n")
 
-	                auth_header = request_split.find{|h| h.match(/Authorization/)}
-					if !auth_header
-					  puts "[WARNING] Unauthorized attempt detected (no header provided)!".red
-	                  connection.close
-	                  Thread.exit
-					end
-					if auth_header
-					  auth_key = auth_header.downcase.gsub("authorization:", "").strip
-					  if auth_key != AUTH_KEY
-					  	puts "[WARNING] Unauthorized attempt detected (wrong auth key)!".red
-						connection.close
-						Thread.exit
-					  end
-					end
-	                
-	                endpoint_host, endpoint_port =  request_split.first.split(" ")[1].split(":")
-	                puts "#{endpoint_host}:#{endpoint_port}".green if OPTIONS[:verbose]
-	                endpoint_connection = TCPSocket.new(endpoint_host, endpoint_port)
-	                endpoint_connection.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true) 
+  if request.match?(/CONNECT/)
 
-	                if endpoint_connection
-	                  connection.puts(CONN_OK)
-	                else
-	                  connection.puts(CONN_FAIL)
-	                  connection.close
-	                  Thread.exit
-	                end
+    auth_header = request_split.find { |h| h.match(/Authorization/) }
+    unless auth_header
+      puts '[WARNING] Unauthorized attempt detected (no header provided)!'.red
+      connection.close
+      Thread.exit
+    end
+    if auth_header
+      auth_key = auth_header.downcase.gsub('authorization:', '').strip
+      if auth_key != AUTH_KEY
+        puts '[WARNING] Unauthorized attempt detected (wrong auth key)!'.red
+        connection.close
+        Thread.exit
+      end
+    end
 
-                    begin
-	                    loop do
-	                            fds = IO.select([connection, endpoint_connection], nil, nil, TTL)
-	                            if fds[0].member?(connection)
-	                              buf = connection.readpartial(MAX_BUFFER)
-	                              endpoint_connection.print(buf)
-	                            elsif fds[0].member?(endpoint_connection)
-	                              buf = endpoint_connection.readpartial(MAX_BUFFER)
-	                              connection.print(buf)
-	                            end
-	                    end
-                     rescue
-	                     puts "[*] Closing connection with #{endpoint_host}:#{endpoint_port}".red if OPTIONS[:verbose]
-	                     endpoint_connection.close() if endpoint_connection
-	                     connection.close() if connection
-	                     Thread.exit
-                     end
+    endpoint_host, endpoint_port = request_split.first.split(' ')[1].split(':')
+    puts "#{endpoint_host}:#{endpoint_port}".green if OPTIONS[:verbose]
+    endpoint_connection = TCPSocket.new(endpoint_host, endpoint_port)
+    endpoint_connection.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
 
-                else
-	               ## GET POST PUT PATCH DELETE ##
-	               host = request_split[1].downcase.gsub("host:", "").strip
-	               endpoint_host, endpoint_port = host.split(":")
-	               endpoint_port = 80 if endpoint_port.nil?
-	               endpoint_port = endpoint_port.to_i
+    if endpoint_connection
+      connection.puts(CONN_OK)
+    else
+      connection.puts(CONN_FAIL)
+      connection.close
+      Thread.exit
+    end
 
-	                if endpoint_host == PUBLIC_IP
-		                response = "HTTP/1.1 200 OK\r\nServer: #{SERVER_NAME}\r\nContent-Type: text/html\r\n\r\n#{File.read('index.html')}"
-		                connection.puts(response)
-		                connection.close
-		                puts "[LOGS] Webpage is shown, closing the connection...".green if OPTIONS[:verbose]
-		                Thread.exit
-	                end
+    begin
+      loop do
+        fds = IO.select([connection, endpoint_connection], nil, nil, TTL)
+        if fds[0].member?(connection)
+          buf = connection.readpartial(MAX_BUFFER)
+          endpoint_connection.print(buf)
+        elsif fds[0].member?(endpoint_connection)
+          buf = endpoint_connection.readpartial(MAX_BUFFER)
+          connection.print(buf)
+        end
+      end
+    rescue StandardError
+      puts "[*] Closing connection with #{endpoint_host}:#{endpoint_port}".red if OPTIONS[:verbose]
+      endpoint_connection.close if endpoint_connection
+      connection.close if connection
+      Thread.exit
+    end
 
-	             	auth_header = request_split.find{|h| h.match(/Authorization/)}
-					if !auth_header
-					  puts "[WARNING] Unauthorized attempt detected (no header provided)!".red
-	                  connection.close
-	                  Thread.exit
-					end
-				    if auth_header
-					  auth_key = auth_header.downcase.gsub("authorization:", "").strip
-					  if auth_key != AUTH_KEY
-					  	puts "[WARNING] Unauthorized attempt detected (wrong auth key)!".red
-						connection.close
-						Thread.exit
-					  end
-					end
+  else
+    ## GET POST PUT PATCH DELETE ##
+    host = request_split[1].downcase.gsub('host:', '').strip
+    endpoint_host, endpoint_port = host.split(':')
+    endpoint_port = 80 if endpoint_port.nil?
+    endpoint_port = endpoint_port.to_i
 
-	               begin
-	               		endpoint_connection = TCPSocket.new(endpoint_host, endpoint_port)
-	               		puts "#{endpoint_host}:#{endpoint_port}".green if OPTIONS[:verbose]
-		           		endpoint_connection.puts(request)
-		           		response = endpoint_connection.readpartial(MAX_BUFFER)
-		           		connection.puts(response)
-		           		connection.close
-	               		Thread.exit
-	               rescue
-	                	puts "#{endpoint_host}:#{endpoint_port}".red if OPTIONS[:verbose]
-	                	connection.puts(CONN_FAIL)
-	                	connection.close if connection
-	                	Thread.exit
-	               end
+    if endpoint_host == PUBLIC_IP
+      response = "HTTP/1.1 200 OK\r\nServer: #{SERVER_NAME}\r\nContent-Type: text/html\r\n\r\n#{File.read('index.html')}"
+      connection.puts(response)
+      connection.close
+      puts '[LOGS] Webpage is shown, closing the connection...'.green if OPTIONS[:verbose]
+      Thread.exit
+    end
 
-                end
+    auth_header = request_split.find { |h| h.match(/Authorization/) }
+    unless auth_header
+      puts '[WARNING] Unauthorized attempt detected (no header provided)!'.red
+      connection.close
+      Thread.exit
+    end
+    if auth_header
+      auth_key = auth_header.downcase.gsub('authorization:', '').strip
+      if auth_key != AUTH_KEY
+        puts '[WARNING] Unauthorized attempt detected (wrong auth key)!'.red
+        connection.close
+        Thread.exit
+      end
+    end
 
+    begin
+      endpoint_connection = TCPSocket.new(endpoint_host, endpoint_port)
+      puts "#{endpoint_host}:#{endpoint_port}".green if OPTIONS[:verbose]
+      endpoint_connection.puts(request)
+      response = endpoint_connection.readpartial(MAX_BUFFER)
+      connection.puts(response)
+      connection.close
+      Thread.exit
+    rescue StandardError
+      puts "#{endpoint_host}:#{endpoint_port}".red if OPTIONS[:verbose]
+      connection.puts(CONN_FAIL)
+      connection.close if connection
+      Thread.exit
+    end
 
+  end
 end
 
 # The main loop
 
 loop do
-        Thread.new(socket.accept) do |connection|
-                begin
-	                tls = OpenSSL::SSL::SSLSocket.new(connection, sslContext)
-	                state = tls.state
-	                Timeout.timeout(10) do
-                    	tls_connection  = tls.accept
-                    	handle_client(tls_connection) if tls_connection
-	                end
-                rescue Timeout::Error
-	                if connection && state == "PINIT" 
-	                	connection.close                
-	                end
-                rescue => e
-	                puts "[ERROR] #{e}".red if OPTIONS[:verbose]
-	                connection.close if connection
-                end
+  Thread.new(socket.accept) do |connection|
+    tls = OpenSSL::SSL::SSLSocket.new(connection, sslContext)
+    tls_connection = nil
 
-        end
+    Timeout.timeout(10) do
+      tls_connection = tls.accept
+    end
+    if tls_connection
+      handle_client(tls_connection) if tls_connection
+    else
+      connection.close
+    end
+  rescue Timeout::Error
+    connection.close if connection && tls.state == 'PINIT'
+  rescue StandardError => e
+    puts "[ERROR] #{e}".red if OPTIONS[:verbose]
+    connection.close if connection
+  end
 end
